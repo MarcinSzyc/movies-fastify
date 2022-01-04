4a233ea8bb666bc059c8b6133e6b2ed7d33acf4b
Marcin Szyc <szyc.marcin@gmai.com>
2022-01-04 13:27:20 +0100

    feat: initial scaffold

---
+++ packages/movies/movies/src/routes/movies/index.ts
@@ -0,0 +1,25 @@
+import { FastifyPluginAsync } from "fastify"
+
+const movies: FastifyPluginAsync = async (fastify): Promise<void> => {
+  const opts = {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['title'],
+        properties: {
+          title: { type: 'string' },
+        }
+      }
+    }
+  }
+
+  fastify.get('/', async function (request, reply) {
+    return 'this is an example'
+  })
+
+  fastify.post('/', opts , async function (request, reply) {
+    return request.body;
+  })
+}
+
+export default movies;
